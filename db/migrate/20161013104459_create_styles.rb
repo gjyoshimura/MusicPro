799d8d192bcf046b5613a43110360f0e5cc9fca4
Claudio Silva <ddk.claudio@gmail.com>
2016-10-15 22:36:26 +0000

    adicaod e atributos

--- db/migrate/20161013104459_create_styles.rb
+++ db/migrate/20161013104459_create_styles.rb
@@ -2,6 +2,8 @@ class CreateStyles < ActiveRecord::Migration
   def change
     create_table :styles do |t|
       t.string :name
+      t.string :litle_description
+      t.string :description
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -66,8 +66,10 @@ ActiveRecord::Schema.define(version: 20161013104815) do
 
   create_table "styles", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "litle_description"
+    t.string   "description"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "topics", force: :cascade do |t|
