3e25cacdf9049af365696e0b3395664ffa1ec43d
Claudio Silva <ddk.claudio@gmail.com>
2016-10-16 22:09:24 +0000

    redirecionar para home apos escolha de estilo e instrumento

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -44,7 +44,7 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to new_profile_has_instrument_path, notice: 'Style was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Style was successfully created.' }
         format.json { render :show, status: :created, location: @profile }
       else
         format.html { render :new }
