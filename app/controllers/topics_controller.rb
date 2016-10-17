13a606ccec0d0867b851da7afb49afabbae2d20b
Claudio Silva <ddk.claudio@gmail.com>
2016-10-17 00:08:13 +0000

    arrumando parte adm

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -71,6 +71,6 @@ class TopicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
-      params.require(:topic).permit(:name, :description)
+      params.require(:topic).permit(:name, :description, :avatar, :style_id, :instrument_id)
     end
 end
--- app/views/lessons/_lesson.json.jbuilder
+++ app/views/lessons/_lesson.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! lesson, :id, :title, :description, :video_url, :author, :topic_id, :created_at, :updated_at
+json.extract! lesson, :id, :name, :description, :video_url, :topic_id, :created_at, :updated_at
 json.url lesson_url(lesson, format: :json)
