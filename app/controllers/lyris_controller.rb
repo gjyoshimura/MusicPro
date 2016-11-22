b11e768c23053de80f27479ec74e687300e76dce
vitor <vitor.samora@gmail.com>
2016-11-22 16:51:23 -0200

    Comentários na tela de vídeo

--- app/controllers/lyris_controller.rb
+++ app/controllers/lyris_controller.rb
@@ -14,6 +14,7 @@ class LyrisController < ApplicationController
 
   def Lesson
     @lesson = Lesson.find(params[:id])
+    @comments = Comment.where("lesson_id = ?", @lesson.id).select("body, user_id")
   end
   
   def StyleDescription
